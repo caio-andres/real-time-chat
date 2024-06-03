b488012c8df2f65a8cdf636f0fa94dd51ba6fb2f
Caio André <caioandresgit@outlook.com>
2024-06-03 07:16:01 -0300

    feat(interface/socket): Interface created, socket connected api with port 3333 to interface (port 5173).

---
+++ chat-interface/src/pages/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Content = styled.div``;
+
+export const Card = styled.div``;
+
+export const MyMessage = styled.div``;
+
+export const OtherMessage = styled.div``;
